4f90293681836d144cd8ec42e2e8357c84b6e28c
flucium <14249224+flucium@users.noreply.github.com>
2023-12-10 10:50:24 +0000

    add ./crates/xenon-common/src/size.rs

---
+++ crates/xenon-common/src/size.rs
@@ -0,0 +1,36 @@
+
+/// 10-byte
+pub const SIZE_10_BYTE:usize = 10;
+
+/// 12-byte
+pub const SIZE_12_BYTE:usize = 12;
+
+/// 16-byte
+pub const SIZE_16_BYTE:usize = 16;
+
+/// 24-byte
+pub const SIZE_24_BYTE:usize = 24;
+
+/// 32-byte
+pub const SIZE_32_BYTE:usize = 32;
+
+/// 56-byte
+pub const SIZE_56_BYTE:usize = 56;
+
+/// 57-byte
+pub const SIZE_57_BYTE:usize = 57;
+
+/// 64-byte
+pub const SIZE_64_BYTE:usize = 64;
+
+/// 114-byte
+pub const SIZE_114_BYTE:usize = 114;
+
+/// 1024-byte
+pub const SIZE_1024_BYTE:usize = 1024;
+
+/// 2048-byte
+pub const SIZE_2048_BYTE:usize = 2048;
+
+/// 4096-byte
+pub const SIZE_4096_BYTE:usize = 4096;
