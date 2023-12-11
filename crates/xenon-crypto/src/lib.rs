d1435087246369b8e7274e723f74ba99002050ec
flucium <14249224+flucium@users.noreply.github.com>
2023-12-11 06:37:38 +0000

    update ./crates/xenon-crypto/src/lib.rs
    added 'pub fn eq(...)...{...}'

--- crates/xenon-crypto/src/lib.rs
+++ crates/xenon-crypto/src/lib.rs
@@ -1,22 +1,49 @@
-mod symmetric;
 mod asymmetric;
-mod key;
-mod fingerprint;
 mod expiry;
+mod fingerprint;
+mod hybrid;
+mod key;
+mod symmetric;
 
 // pub mod
-pub mod rand;
-pub mod hash;
 pub mod algorithm;
+pub mod hash;
+pub mod rand;
 
 // pub use
-pub use symmetric::*;
 pub use asymmetric::*;
-pub use key::*;
-pub use fingerprint::*;
 pub use expiry::*;
+pub use fingerprint::*;
+pub use hybrid::*;
+pub use key::*;
+pub use symmetric::*;
 
 // Re-export
+pub use chrono::{NaiveDate as Date, Utc};
 pub use openssl;
 pub use uuid::Uuid;
-pub use chrono::{NaiveDate as Date, Utc};
+
+
+/// Compare two byte slices in constant time.
+/// 
+/// # Example
+/// ```
+/// use xenon_crypto::eq;
+/// 
+/// let a = b"Hello";
+/// 
+/// let b = b"Hello";
+/// 
+/// assert!(eq(a, b));
+/// ```
+pub fn eq(a: impl AsRef<[u8]>, b: impl AsRef<[u8]>) -> bool {
+    let a = a.as_ref();
+
+    let b = b.as_ref();
+
+    if a.len() != b.len() {
+        return false;
+    }
+
+    openssl::memcmp::eq(a, b)
+}
