fb28c686290fcfbc161d06cfe9942068bba561d7
flucium <14249224+flucium@users.noreply.github.com>
2023-12-11 04:49:10 +0000

    update ./crates/xenon-crypto/src/rand.rs
    add gen::<>(){}

--- crates/xenon-crypto/src/rand.rs
+++ crates/xenon-crypto/src/rand.rs
@@ -1,11 +1,13 @@
-use xenon_common::{size::{SIZE_12_BYTE, SIZE_16_BYTE, SIZE_24_BYTE, SIZE_32_BYTE}, Error, Result};
+use xenon_common::{
+    size::{SIZE_12_BYTE, SIZE_16_BYTE, SIZE_24_BYTE, SIZE_32_BYTE},
+    Error, ErrorKind, Result,
+};
 
 /// Generate cryptographically strong pseudo-random bytes
 pub fn gen_12() -> Result<[u8; SIZE_12_BYTE]> {
     let mut buffer = [0u8; SIZE_12_BYTE];
 
-    openssl::rand::rand_bytes(&mut buffer)
-        .map_err(|_| Error::internal_error())?;
+    openssl::rand::rand_bytes(&mut buffer).map_err(|_| Error::internal_error())?;
 
     Ok(buffer)
 }
@@ -14,8 +16,7 @@ pub fn gen_12() -> Result<[u8; SIZE_12_BYTE]> {
 pub fn gen_16() -> Result<[u8; SIZE_16_BYTE]> {
     let mut buffer = [0u8; SIZE_16_BYTE];
 
-    openssl::rand::rand_bytes(&mut buffer)
-        .map_err(|_| Error::internal_error())?;
+    openssl::rand::rand_bytes(&mut buffer).map_err(|_| Error::internal_error())?;
 
     Ok(buffer)
 }
@@ -24,8 +25,7 @@ pub fn gen_16() -> Result<[u8; SIZE_16_BYTE]> {
 pub fn gen_24() -> Result<[u8; SIZE_24_BYTE]> {
     let mut buffer = [0u8; SIZE_24_BYTE];
 
-    openssl::rand::rand_bytes(&mut buffer)
-        .map_err(|_| Error::internal_error())?;
+    openssl::rand::rand_bytes(&mut buffer).map_err(|_| Error::internal_error())?;
 
     Ok(buffer)
 }
@@ -34,8 +34,29 @@ pub fn gen_24() -> Result<[u8; SIZE_24_BYTE]> {
 pub fn gen_32() -> Result<[u8; SIZE_32_BYTE]> {
     let mut buffer = [0u8; SIZE_32_BYTE];
 
-    openssl::rand::rand_bytes(&mut buffer)
-        .map_err(|_| Error::internal_error())?;
+    openssl::rand::rand_bytes(&mut buffer).map_err(|_| Error::internal_error())?;
+
+    Ok(buffer)
+}
+
+pub fn gen<const T: usize>() -> Result<[u8; T]> {
+    if T == 0 {
+        Err(Error::new(
+            ErrorKind::InvalidLength,
+            String::from("The minimum size is 1-byte"),
+        ))?;
+    }
+
+    if T > SIZE_32_BYTE {
+        Err(Error::new(
+            ErrorKind::InvalidLength,
+            String::from("The maximum size is 32-bytes"),
+        ))?;
+    }
+
+    let mut buffer = [0u8; T];
+
+    openssl::rand::rand_bytes(&mut buffer).map_err(|_| Error::internal_error())?;
 
     Ok(buffer)
 }
@@ -50,7 +71,7 @@ pub fn gen_32() -> Result<[u8; SIZE_32_BYTE]> {
     Generate 16
     Generate 24
     Generate 32
-    
+
 */
 
 #[test]
