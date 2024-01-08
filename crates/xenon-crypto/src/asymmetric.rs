91993c5b2db8edb0233bd00ae34ad41ada34dbf3
flucium <flucium@flucium.net>
2024-01-08 05:09:43 +0000

    fix ./crates/xenon-crypto/src/asymmetric.rs
    Fixed: error when encapsulating shared_secret into SymmetricKey. 'symmetric key len err'

--- crates/xenon-crypto/src/asymmetric.rs
+++ crates/xenon-crypto/src/asymmetric.rs
@@ -1,20 +1,23 @@
+pub(super) mod curve25519;
+pub(super) mod curve448;
+
+mod dh;
+mod signer;
+mod verifier;
+
 use crate::algorithm::Hasher;
+use crate::curve25519::{x25519_diffie_hellman, x25519_diffie_hellman_ephemeral};
+use crate::curve448::{x448_diffie_hellman, x448_diffie_hellman_ephemeral};
 use crate::hash::{sha2::*, sha3::*};
 use crate::{
     algorithm::Asymmetric,
     curve25519::{ed25519_sign, ed25519_verify},
     curve448::{ed448_sign, ed448_verify},
-    Key, PrivateKey,
+    Key, PrivateKey, PublicKey, Symmetric, SymmetricKey, Utc, Uuid,
 };
-use crate::{PublicKey, Utc, Uuid};
-use xenon_common::{Error, ErrorKind, Result};
 
-pub(super) mod curve25519;
-pub(super) mod curve448;
-mod dh;
-mod signer;
-mod verifier;
-// mod curve448;
+use xenon_common::size::{SIZE_16_BYTE, SIZE_24_BYTE, SIZE_32_BYTE};
+use xenon_common::{Error, ErrorKind, Result};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Signature {
@@ -318,4 +321,261 @@ pub fn sign(private_key: &PrivateKey, hasher: Hasher, message: &[u8]) -> Result<
         bytes,
         timestamp,
     })
+}
+
+/// Diffie-Hellman Key Exchange
+///
+/// **!! Under development !!**
+pub fn diffie_hellma(
+    private_key: &PrivateKey,
+    public_key: &PublicKey,
+    symmetric: Symmetric,
+) -> Result<SymmetricKey> {
+    is_private_key_expired(private_key)?;
+
+    is_public_key_expired(public_key)?;
+
+    is_match_algorithm(private_key, public_key)?;
+
+    let algorithm = private_key.algorithm().try_into().unwrap();
+
+    let shared_secret = match algorithm {
+        Asymmetric::X25519 => {
+            let private_key = private_key.as_bytes().try_into().unwrap();
+
+            let public_key = public_key.as_bytes().try_into().unwrap();
+
+            x25519_diffie_hellman(private_key, public_key)
+                .map_err(|_| Error::internal_error())?
+                .to_vec()
+        }
+        Asymmetric::X448 => {
+            let private_key = private_key.as_bytes().try_into().unwrap();
+
+            let public_key = public_key.as_bytes().try_into().unwrap();
+
+            x448_diffie_hellman(private_key, public_key)
+                .map_err(|_| Error::internal_error())?
+                .to_vec()
+        }
+        _ => Err(Error::new(
+            ErrorKind::Unsupported,
+            String::from("Unsupported algorithm"),
+        ))?,
+    };
+
+    to_symmetric_key(symmetric, &shared_secret)
+}
+
+/// Diffie-Hellman Key Exchange (Ephemeral)
+///
+/// **!! Under development !!**
+pub fn diffie_hellma_ephemeral(
+    their_public: &PublicKey,
+    symmetric: Symmetric,
+) -> Result<(PublicKey, SymmetricKey)> {
+    is_public_key_expired(their_public)?;
+
+    let algorithm = their_public.algorithm().try_into().unwrap();
+
+    let (public_key, shared_secret) = match algorithm {
+        Asymmetric::X25519 => {
+            let public_key = their_public.as_bytes().try_into().unwrap();
+
+            let (public_key, shared_secret) =
+                x25519_diffie_hellman_ephemeral(public_key).map_err(|_| Error::internal_error())?;
+
+            let public_key = PublicKey::new_from_slice(
+                their_public.algorithm().try_into().unwrap(),
+                &public_key,
+            )?;
+
+            (public_key, shared_secret.to_vec())
+        }
+        Asymmetric::X448 => {
+            let public_key = their_public.as_bytes().try_into().unwrap();
+
+            let (public_key, shared_secret) =
+                x448_diffie_hellman_ephemeral(public_key).map_err(|_| Error::internal_error())?;
+
+            let public_key = PublicKey::new_from_slice(
+                their_public.algorithm().try_into().unwrap(),
+                &public_key,
+            )?;
+
+            (public_key, shared_secret.to_vec())
+        }
+        _ => Err(Error::new(
+            ErrorKind::Unsupported,
+            String::from("Unsupported algorithm"),
+        ))?,
+    };
+
+    let symmetric_key = to_symmetric_key(symmetric, &shared_secret)?;
+
+    Ok((public_key, symmetric_key))
+}
+
+#[inline]
+fn to_symmetric_key(symmetric: Symmetric, shared_secret: &[u8]) -> Result<SymmetricKey> {
+    let key_length = match symmetric {
+        Symmetric::Aes128Gcm => SIZE_16_BYTE,
+        Symmetric::Aes192Gcm => SIZE_24_BYTE,
+        Symmetric::Aes256Gcm => SIZE_32_BYTE,
+        Symmetric::ChaCha20Poly1305 => SIZE_32_BYTE,
+    };
+
+    let key_bytes = &shared_secret[..key_length];
+
+    SymmetricKey::new_from_slice(symmetric, key_bytes)
+}
+
+#[inline]
+fn is_match_algorithm(private_key: &PrivateKey, public_key: &PublicKey) -> Result<()> {
+    if private_key.algorithm() == public_key.algorithm() {
+        Ok(())
+    } else {
+        Err(Error::new(
+            ErrorKind::Unsupported,
+            String::from("Private key and public key must be the same algorithm"),
+        ))?
+    }
+}
+
+#[inline]
+fn is_public_key_expired(public_key: &PublicKey) -> Result<()> {
+    if public_key.expiry().is_expired() {
+        Ok(())
+    } else {
+        Err(Error::new(
+            ErrorKind::Expired,
+            String::from("Public key is expired"),
+        ))
+    }
+}
+
+#[inline]
+fn is_private_key_expired(private_key: &PrivateKey) -> Result<()> {
+    if private_key.expiry().is_expired() {
+        Ok(())
+    } else {
+        Err(Error::new(
+            ErrorKind::Expired,
+            String::from("Private key is expired"),
+        ))
+    }
+}
+
+/*
+    Unit tests
+
+    X25519
+    X448
+
+    e.g.
+    cargo test --package xenon-crypto --lib -- asymmetric::test_x25519 --exact --nocapture
+*/
+
+#[test]
+fn test_x25519() {
+    let private_key = PrivateKey::generate(Asymmetric::X25519).unwrap();
+
+    let public_key = PublicKey::from_private_key(&private_key).unwrap();
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::Aes128Gcm).is_ok(),
+        true
+    );
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::Aes192Gcm).is_ok(),
+        true
+    );
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::Aes256Gcm).is_ok(),
+        true
+    );
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::ChaCha20Poly1305).is_ok(),
+        true
+    );
+}
+
+#[test]
+fn test_x448() {
+    let private_key = PrivateKey::generate(Asymmetric::X448).unwrap();
+
+    let public_key = PublicKey::from_private_key(&private_key).unwrap();
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::Aes128Gcm).is_ok(),
+        true
+    );
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::Aes192Gcm).is_ok(),
+        true
+    );
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::Aes256Gcm).is_ok(),
+        true
+    );
+
+    assert_eq!(
+        diffie_hellma(&private_key, &public_key, Symmetric::ChaCha20Poly1305).is_ok(),
+        true
+    );
+}
+
+#[test]
+fn test_x25519_ephemeral() {
+    let private_key = PrivateKey::generate(Asymmetric::X25519).unwrap();
+    let their_public = PublicKey::from_private_key(&private_key).unwrap();
+
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::Aes128Gcm).is_ok(),
+        true
+    );
+
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::Aes192Gcm).is_ok(),
+        true
+    );
+    
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::Aes256Gcm).is_ok(),
+        true
+    );
+    
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::ChaCha20Poly1305).is_ok(),
+        true
+    );
+}
+
+#[test]
+fn test_x448_ephemeral() {
+    let private_key = PrivateKey::generate(Asymmetric::X448).unwrap();
+    
+    let their_public = PublicKey::from_private_key(&private_key).unwrap();
+    
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::Aes128Gcm).is_ok(),
+        true
+    );
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::Aes192Gcm).is_ok(),
+        true
+    );
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::Aes256Gcm).is_ok(),
+        true
+    );
+    assert_eq!(
+        diffie_hellma_ephemeral(&their_public, Symmetric::ChaCha20Poly1305).is_ok(),
+        true
+    );
 }
