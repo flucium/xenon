cfcf158f4b70899fe7789b3d74a8977b1cd1f804
flucium <flucium@flucium.net>
2024-02-06 06:04:19 +0000

    clean ./crates/xenon-crypto/src/key

--- crates/xenon-crypto/src/key/mod.rs
+++ crates/xenon-crypto/src/key/mod.rs
@@ -3,11 +3,9 @@ mod ed25519;
 mod x25519;
 mod symmetric;
 mod aes;
-// mod chacha20;
 
 pub use asymmetric::*;
 pub use ed25519::*;
 pub use x25519::*;
 pub use symmetric::*;
 pub use aes::*;
-// pub use chacha20::*;
